87dce5e0bc9fdae25ae0f28e179bcb6d1ffb1cc2
Andreas Pfohl <mail@apfohl.com>
2015-07-09 10:10:23 +0200

    Added external memory (not in use)

---
+++ memory.c
@@ -0,0 +1,3 @@
+#include "memory.h"
+
+uint8_t memory[MEMORY_SIZE];
---
+++ memory.h
@@ -0,0 +1,10 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <stdint.h>
+
+static const int MEMORY_SIZE = 65536;
+
+extern uint8_t memory[MEMORY_SIZE];
+
+#endif /* end of include guard: MEMORY_H */
